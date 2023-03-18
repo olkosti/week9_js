22f42f329970d5244fb62aa27264ff59fe8e0a21
unknown <kov1606@gmail.com>
2023-03-18 20:16:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//Задача №1
+let salaryСleaner = 40000;
+let salaryTeacher = 22000;
+console.log(`Вечерний уборщик, имея голос и педагогическое образование, сможет зарабатывать ${salaryСleaner + salaryTeacher} рублей в месяц`);
+
+//Задача №2
+console.log('Владивосток ' + 2000);
+
+//Задача №3
+let messageMinutes = 128;
+let likeFotoMinutes = 254;
+let VkMinutes = 137;
+let YouTubeMinutes = 201;
+const minutesInHour = 60;
+console.log(`Русалка прокрастинировала ${(messageMinutes + likeFotoMinutes + VkMinutes + YouTubeMinutes) / minutesInHour} часов`)
+
+//Задача №4
+console.log(2**5);
+
+//Задача №5
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+
+//Задача №6
+const greetings = "Привет";
+const name = "Кот";
+console.log(`${greetings}, ${name}`);
+
+//Задача №7
+console.log('Задача №7:');
+let time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time = 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву.');
+
+//Задача №8
+console.log('Задача №8:');
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
