f6574c72764ecb3b796518be6b3dccc8913c8296
unknown <kov1606@gmail.com>
2023-03-19 19:13:40 +0300

    2 commit

--- index.js
+++ index.js
@@ -27,11 +27,13 @@ console.log(`${greetings}, ${name}`);
 
 //Задача №7
 console.log('Задача №7:');
-let time = 34;
+let time;
+console.log(time);
+time = 34;
 console.log('Старт поездки. Осталось минут: ' + time);
-time = 15;
+time -= 15;
 console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
-time = 10;
+time -= 10;
 console.log('Немного не новостей, но событий. Осталось минут: ' + time);
 time = 0;
 console.log('Вы приехали. Добро пожаловать в Москву.');
